589fdef7fb8d2813ad188ba441eb429f393fe6b9
Nyxeia <melothibeault@gmail.com>
2025-05-10 20:45:29 -0400

    base kinda working

--- JavaScript/Champs de profil nyx.js
+++ JavaScript/Champs de profil nyx.js
@@ -1,103 +1,110 @@
-var selectedColor = {
-  "text-transform" : "uppercase"
-};
-  
-var defaultColor = {
-  "text-transform" : "lowercase"
-};
-  
-  $( function(){
-    $('body')
-      .on('click', '.as-above', function(){
+$(function() {
+  // AS ABOVE
+  $('body').on('click', '.as-above', function() {
+      $('.tabcontent1').show();
+      $('.tabcontent2').hide();
 
-        var above = $(this).nextAll(".tabcontent:first");
-        var below = $(this).nextAll(".tabcontent2:first");
+      // Show AS ABOVE elements
+      $('.custom-icon').show();
+      $('.custom-name').show();
+      $('.custom-rank').show();
+      
+      // Hide SO BELOW elements
+      $('.custom-icon-hrp').hide();
+      $('.custom-fc').hide();
+      $('.custom-pseudo').hide();
 
-        if (below.is(":visible")) {
-  
-          below.hide();
-          above.show();
-  
-          $(this).css(selectedColor);
-          $(this).nextAll(".so-below:first").css(defaultColor);
-        }
+      // Update border colors
+      $('.tabs-wrap').css({
+          'border-left': '3px solid var(--accent1)',
+          'border-right': '3px solid var(--neutral4)'
       });
   });
   
-  $( function(){
-    $('body')
-      .on('click', '.so-below', function(){
-  
-      var above = $(this).nextAll(".tabcontent:first");
-      var below = $(this).nextAll(".tabcontent2:first");
-  
-        if (above.is(":visible")) {
-  
-          above.hide();
-          below.show();
-  
-          $(this).css(selectedColor);
-          $(this).prevAll(".as-above:first").css(defaultColor);
-        }
-  
+  // SO BELOW
+  $('body').on('click', '.so-below', function() {
+      $('.tabcontent1').hide();
+      $('.tabcontent2').show();
+
+      // Hide AS ABOVE elements
+      $('.custom-icon').hide();
+      $('.custom-name').hide();
+      $('.custom-rank').hide();
+      
+      // Show SO BELOW elements
+      $('.custom-icon-hrp').show();
+      $('.custom-fc').show();
+      $('.custom-pseudo').show();
+
+      // Update border colors
+      $('.tabs-wrap').css({
+      'border-left': '3px solid var(--neutral4)',
+      'border-right': '3px solid var(--accent1)'
       });
   });
-  
-  
-jQuery(function($) {
-  var path = ".post_profile .post_userinfo .tabcontent1 .user_field";
-  var parent = ".post_profile";
-  
-  // Configuration: fields to move and fields to hide labels
-  var fieldsToMove = [
-      "Avatar", "Credits", "Messages", "Date d'inscription",
-      "Triggers", "Warnings", "Pseudo", "Multicomptes",
-      "Habitudes RPG", "Points"
-  ];
-  
-  var fieldsToHideLabel = ["Icone", "Zone libre 1", "Zone libre 2"];
-  
-  $(path).each(function() {
-      var $field = $(this);
-      var $label = $field.find('.field_label .label');
-      var fieldName = $label.text().replace(':', '').trim();
-      
-      // Move field to tabcontent2 if it's in the move list
-      if (fieldsToMove.includes(fieldName)) {
-          var $tabcontent2 = $field.closest(parent).find('.tabcontent2');
-          $tabcontent2.append($field);
-      }
-      
-      // Handle the Icone field specially
-      if (fieldName === "Icone") {
-          // Find the image in this field
-          var $image = $field.find('.field_content img');
-          
-          if ($image.length > 0) {
-              // Find the custom-icon div in the same post profile
-              var $customIcon = $field.closest('.post_profile').find('.custom-icon');
-              
-              // Move the image to the custom-icon div
-              if ($customIcon.length > 0) {
-                  $customIcon.append($image);
-              }
-          }
-          
-          // Remove the entire Icone field since we've moved its content
-          $field.remove();
-      }
-      // Handle other fields that need their labels hidden
-      else if (fieldsToHideLabel.includes(fieldName)) {
-          // Remove the entire label element
-          $field.find('.field_label').remove();
-      }
-  });
-  
-  // Additional cleanup for any remaining labels
-  $('.field_label .label').each(function() {
-      var text = $(this).text().replace(':', '').trim();
-      if (fieldsToHideLabel.includes(text)) {
-          $(this).parent('.field_label').remove();
-      }
-  });
+});
+
+
+  jQuery(function($) {
+    var path = ".post_profile .post_userinfo .tabcontent1 .user_field";
+    var parent = ".post_profile";
+    
+    // Move to tabcontent2
+    var fieldsToMove = [
+        "Credits", "Messages", "Date d'inscription",
+        "Triggers", "Warnings", "Multicomptes",
+        "Habitudes RPG", "Points"
+    ];
+    
+    var customDestinations = {
+        "Icone": ".custom-icon",
+        "Autre identité": ".custom-name",
+        "Rang personnalisé": ".custom-rank",
+        "Icone HRP": ".custom-icon-hrp",
+        "Faceclaim": ".custom-fc",
+        "Pseudo": ".custom-pseudo"
+    };
+    
+    // Only hide labels (without moving)
+    var fieldsToHideLabel = ["Zone libre 1", "Zone libre 2"];
+    
+    $(path).each(function() {
+        var $field = $(this);
+        var $label = $field.find('.field_label > .label');
+        var fieldName = $label.text().replace(':', '').trim();
+        
+        // Check if this field has a custom destination
+        if (customDestinations[fieldName]) {
+            var $postProfile = $field.closest('.post_profile');
+            var destinationSelector = customDestinations[fieldName];
+            var $destination = $postProfile.find(destinationSelector);
+            
+            if ($destination.length > 0) {
+
+                var $fieldContent = $field.find('.field_content');
+                var $contentToMove = $fieldContent.html();
+                $destination.html($contentToMove);
+            }
+            
+            // Remove the entire field after moving its content
+            $field.remove();
+        }
+        // Check if field should move to tabcontent2
+        else if (fieldsToMove.includes(fieldName)) {
+            var $tabcontent2 = $field.closest(parent).find('.tabcontent2');
+            $tabcontent2.append($field);
+        }
+        // Check if field should just have its label hidden
+        else if (fieldsToHideLabel.includes(fieldName)) {
+            $field.find('.field_label').remove();
+        }
+    });
+    
+    // Additional cleanup for any remaining labels
+    $('.field_label .label').each(function() {
+        var text = $(this).text().replace(':', '').trim();
+        if (fieldsToHideLabel.includes(text)) {
+            $(this).parent('.field_label').remove();
+        }
+    });
 });
