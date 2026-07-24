a81719624ff15f2a1079a88d63339e7d5602c0be
Nyxeia <aeterna_nocte@hotmail.com>
2026-07-24 12:49:01 -0400

    Missing imgbox

--- JavaScript/Replace.js
+++ JavaScript/Replace.js
@@ -11,4 +11,61 @@ $(function() {
     topicType.find('strong:contains("Note:")').text('Note ✧');
   });
 
+
+  // CODE BY NYXEIA
+
+document.addEventListener('DOMContentLoaded', () => {
+
+var noNewTemp = "https://2img.net/images2.imgbox.com/0d/34/aFa4DuZo_o.png";
+var newTemp = "https://2img.net/images2.imgbox.com/cc/10/GpTcUBdJ_o.gif";
+
+var currentCatImgList = document.getElementsByClassName("forum-img");
+var currentMsgImgList = document.getElementsByClassName("topicslist-img");
+var currentMPImgList = document.getElementsByClassName("mpimg");
+
+// Add class to category img 
+for (var currentImg of currentCatImgList) {
   
+    switch (currentImg.getAttribute("src"))
+    {
+        case newTemp:
+            currentImg.classList.add("img-new");
+            break;
+        case noNewTemp: 
+        default:
+            currentImg.classList.add("img-nonew");
+            break;
+    }
+}
+
+// Same for new message in topic
+for (var currentDiv of currentMsgImgList) {
+    var currentImg = currentDiv.firstChild;
+
+    switch (currentImg.getAttribute("src"))
+    {
+        case newTemp: 
+            currentImg.classList.add("img-new");
+            break;
+        case noNewTemp: 
+        default:
+            currentImg.classList.add("img-nonew");
+            break;
+    }
+}
+
+// Same for new message in private
+for (var currentImg of currentMPImgList) {
+
+    switch (currentImg.getAttribute("src"))
+    {
+        case newTemp: 
+            currentImg.classList.add("img-new");
+            break;
+        case noNewTemp: 
+        default:
+            currentImg.classList.add("img-nonew");
+            break;
+    }
+}
+});
